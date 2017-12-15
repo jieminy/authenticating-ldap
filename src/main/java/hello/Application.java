0715f402145e1d882f5fc3486636b48e95219a04
jiemin <540959120@qq.com>
2017-12-15 14:44:08 +0800

    spring security 配置ssl

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,37 +1,26 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.naming.Context;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
-//    @Value("${ldap.url}")
-//    private String ldapUrl;
-//
-//    @Value("${ldap.base}")
-//    private String ldapBase;
-//
-//    @Value("${ldap.userDn}")
-//    private String ldapUserDn;
-//
-//    @Value("${ldap.userPwd}")
-//    private String ldapUserPwd;
-//
-//    @Bean
-//    public LdapTemplate ldapTemplate(){
-//        return new LdapTemplate(contextSource());
-//    }
 
     @Bean
     @ConfigurationProperties(prefix = "ldap.contextSource")
     public LdapContextSource contextSource(){
         LdapContextSource contextSource = new LdapContextSource();
+        HashMap<String,Object> env = new HashMap<>();
+        env.put("java.naming.ldap.factory.socket",this.getClass().getPackage().getName()+".DummySSLSocketFactory");
+        env.put(Context.SECURITY_PROTOCOL,"ssl");
+        contextSource.setBaseEnvironmentProperties(env);
         return  contextSource;
     }
 
@@ -39,10 +28,7 @@ public class Application {
     public LdapTemplate ldapTemplate(LdapContextSource contextSource){
         return new LdapTemplate(contextSource);
     }
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private LdapContextSource ldapContextSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -27,12 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.userSearchBase("ou=Users")
 				.groupSearchBase("ou=UserRelations")
 				.groupSearchFilter("member={0}")
-//				.groupRoleAttribute("ROLE_")
-				.contextSource()
-					.url("ldap://localhost:389/dc=maxcrc,dc=com")
-				    .managerDn("cn=Manager,dc=maxcrc,dc=com")
-					.managerPassword("root")
-					.and()
+				.contextSource(ldapContextSource)
 				.passwordCompare()
 					.passwordAttribute("userPassword");
 	}
